859826040e9c3e672d9b167eedee3d27ae0a9246
Roger Rudin <roger.rudin@gmail.com>
2024-10-31 22:48:35 +0100

    feat: dumb component refactored

--- src/app/features/blog-overview-page/blog-overview-page.component.ts
+++ src/app/features/blog-overview-page/blog-overview-page.component.ts
@@ -1,8 +1,5 @@
-import { NgIf, AsyncPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
+import { BlogOverviewCardComponent } from '../../shared/blog-overview-card/blog-overview-card.component';
 
 type Model = {
   data: {
@@ -25,7 +22,7 @@ type Model = {
 @Component({
   selector: 'app-blog-overview-page',
   standalone: true,
-  imports: [NgIf, AsyncPipe, MatCardModule, MatButtonModule, MatIcon],
+  imports: [BlogOverviewCardComponent],
   templateUrl: './blog-overview-page.component.html',
   styleUrl: './blog-overview-page.component.scss',
 })
