e6ad63d5f8126a42c802d9f9be1b22e22a9b06c4
Roger Rudin <roger.rudin@gmail.com>
2024-12-11 22:48:30 +0100

    feat: enhance HTTP client configuration with interceptors for authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,21 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { authConfig } from './core/auth/auth.config';
-import { provideAuth } from 'angular-auth-oidc-client';
+import { AuthInterceptor, provideAuth } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(APP_ROUTES, withComponentInputBinding()),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAuth(authConfig),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
